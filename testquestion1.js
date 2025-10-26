896ca1bc93c693d91ddfe28d9160231c7f99f9dd
toofwess <darius.lim.rx@gmail.com>
2025-10-26 03:42:12 +0000

    done

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,18 @@
+let bird;
+let background;
+let birdImg;
 
+function preload(){
+   background = loadImage("assets/background.png");
+   birdImg = loadImage("assets/bird.png");
+}
+function setup(){
+    new Canvas(400, 600);
+    square = createSprite(width / 2, height / 2, 50, 50);
+    square.img = birdImg;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,20 @@
 
+let square;
+
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+ world.gravity.y = 10;
+    square = new Sprite(200/2,300/2,20,20);
+    // code to create a new square sprite
+}
+
+function draw(){
+    background("grey");
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+        if(mouse.presses()){
+        square.vel.y = -5;}
+}
